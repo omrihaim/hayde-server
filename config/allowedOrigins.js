5e93328081b64c44e34ea3228de87b193b477e63
Omri Haim <3135094+omrihaim@users.noreply.github.com>
2023-03-17 14:48:19 +0000

    CPRS config mistake

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:4200',
     'https://plusachad.com',
-    'https://hayde-web.herokuapp.com/'
+    'https://hayde-web.herokuapp.com'
 ];
 
 module.exports = allowedOrigins;
